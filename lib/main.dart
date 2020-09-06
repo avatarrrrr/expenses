4d82589c62b0dd57c3ea19b33d9a51d9bb6083a2
require64bits <tankoaraujo@gmail.com>
2020-09-06 07:40:37 -0300

    Transacions model implementation

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:expenses/models/transactions.dart';
 import 'package:flutter/material.dart';
 
 main() => runApp(ExpensesApp());
@@ -12,6 +13,13 @@ class ExpensesApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final _transactions = [
+    Transaction(
+        id: 't1', title: 'New Tenis', value: 99.9, date: DateTime.now()),
+    Transaction(
+        id: 't2', title: 'New Shoes', value: 145.5, date: DateTime.now()),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,16 +27,22 @@ class MyHomePage extends StatelessWidget {
         title: Text('Pessoal Expenses'),
       ),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           Container(
-            width: double.infinity,
             child: Card(
               child: Text('Graph'),
               elevation: 5,
               color: Colors.blue,
             ),
           ),
-          Card(child: Text('List of Transactions')),
+          Column(
+              children: _transactions
+                  .map(
+                    (tr) => Card(child: Text(tr.title)),
+                  )
+                  .toList()),
         ],
       ),
     );
